aa631ffb09d9fc5544921246f67667ef47837d5f
“Jasionf” <2248831014@qq.com>
2026-02-25 10:36:00 +0800

    modify

--- components/pyramidrgb/output/__init__.py
+++ components/pyramidrgb/output/__init__.py
@@ -1,35 +1,45 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import output
+from esphome.components import output, i2c
 from esphome.const import CONF_ID, CONF_CHANNEL
-from .. import pyramidrgb_ns, CONF_PYRAMIDRGB_ID, BASE_SCHEMA
 
 CODEOWNERS = ["@Jasionf"]
-DEPENDENCIES = ["pyramidrgb"]
+DEPENDENCIES = ["pyramidrgb", "i2c"]
 
+
+pyramidrgb_ns = cg.esphome_ns.namespace("pyramidrgb")
+PyramidRGBComponent = pyramidrgb_ns.class_("PyramidRGBComponent", cg.Component, i2c.I2CDevice)
 PyramidRGBOutput = pyramidrgb_ns.class_("PyramidRGBOutput", output.FloatOutput)
-RGBColorChannel = pyramidrgb_ns.enum("RGBColorChannel", is_class=True)
 
+#
+CONF_PYRAMIDRGB_ID = "pyramidrgb_id"
+BASE_SCHEMA = cv.Schema({
+	cv.GenerateID(CONF_PYRAMIDRGB_ID): cv.use_id(PyramidRGBComponent),
+})
+
+
+RGBColorChannel = pyramidrgb_ns.enum("RGBColorChannel", is_class=True)
 COLOR_MAP = {
-    "red": RGBColorChannel.COLOR_R,
-    "green": RGBColorChannel.COLOR_G,
-    "blue": RGBColorChannel.COLOR_B,
+	"red": RGBColorChannel.COLOR_R,
+	"green": RGBColorChannel.COLOR_G,
+	"blue": RGBColorChannel.COLOR_B,
 }
 
 CONF_COLOR = "color"
 
+
 CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
-    {
-        cv.Required(CONF_ID): cv.declare_id(PyramidRGBOutput),
-        cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=3),
-        cv.Required(CONF_COLOR): cv.enum(COLOR_MAP),
-    }
+	{
+		cv.Required(CONF_ID): cv.declare_id(PyramidRGBOutput),
+		cv.Required(CONF_CHANNEL): cv.int_range(min=0, max=3),
+		cv.Required(CONF_COLOR): cv.enum(COLOR_MAP),
+	}
 ).extend(BASE_SCHEMA)
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_parented(var, config[CONF_PYRAMIDRGB_ID])
-    cg.add(var.set_channel(config[CONF_CHANNEL]))
-    cg.add(var.set_color(config[CONF_COLOR]))
-    await output.register_output(var, config)
+	var = cg.new_Pvariable(config[CONF_ID])
+	await cg.register_parented(var, config[CONF_PYRAMIDRGB_ID])
+	cg.add(var.set_channel(config[CONF_CHANNEL]))
+	cg.add(var.set_color(config[CONF_COLOR]))
+	await output.register_output(var, config)
--- components/pyramidrgb/output/pyramidrgb_output.h
+++ components/pyramidrgb/output/pyramidrgb_output.h
@@ -7,19 +7,19 @@ namespace esphome {
 namespace pyramidrgb {
 
 class PyramidRGBOutput : public output::FloatOutput,
-                         public Parented<PyramidRGBComponent> {
+                                                 public Parented<PyramidRGBComponent> {
  public:
-  void set_channel(uint8_t channel) { channel_ = channel; }
-  void set_color(RGBColorChannel color) { color_ = color; }
+    void set_channel(uint8_t channel) { channel_ = channel; }
+    void set_color(RGBColorChannel color) { color_ = color; }
 
-  void write_state(float state) override {
-    uint8_t val = this->parent_->map_level(this->color_, state);
-    this->parent_->set_channel_color_component(this->channel_, this->color_, val);
-  }
+    void write_state(float state) override {
+        uint8_t val = (uint8_t) (state <= 0 ? 0 : (state >= 1.0f ? 255 : (state * 255.0f)));
+        this->parent_->set_channel_color_component(this->channel_, this->color_, val);
+    }
 
  protected:
-  uint8_t channel_ {0};
-  RGBColorChannel color_ {COLOR_R};
+    uint8_t channel_ {0};
+    RGBColorChannel color_ {COLOR_R};
 };
 
 }  // namespace pyramidrgb
